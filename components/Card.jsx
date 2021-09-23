7391bd5fd0eeedc843bab3a4558301d292b9f097
Jan Delmar <jandelmardesign@gmail.com>
2021-09-23 21:39:21 +0200

    v4

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,15 +1,14 @@
 import Image from 'next/image'
 
-export default function Card({front, back, alt_front = "alt front", alt_back = "alt back"}) {
+export default function Card({front, back, alt_front = "front", alt_back = "back"}) {
     return (
         <div className="col-md-4 card-container">
           <div className="card-flip">
             <div className="card front">
-              {/* TODO: remove width/height and replace with import local image */}
-              <Image src={front} width="500" height="333" alt={alt_front} className="card-img-top img-fluid" />
+              <Image src={front} alt={alt_front} className="card-img-top img-fluid" />
             </div>
             <div className="card back">
-              <Image src={back} width="500" height="333" alt={alt_back} className="card-img-top img-fluid" />
+              <Image src={back} alt={alt_back} className="card-img-top img-fluid" />
             </div>
           </div>
         </div>
--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,27 +1,24 @@
 import Card from '../components/Card'
-
-const images = [
-  "https://images.unsplash.com/photo-1632134346410-fb65b96d6595?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-  "https://images.unsplash.com/photo-1631992781019-d21eace7d3cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-  "https://images.unsplash.com/photo-1631995191030-1e3c9bb9e528?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-  "https://images.unsplash.com/photo-1463681457836-cc9f3f99608b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-  "https://images.unsplash.com/photo-1576679833928-f6c778b4a864?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-  "https://images.unsplash.com/photo-1631478409245-d8f0e2c7d680?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-]
+import pic1 from '../public/img/1.jpg'
+import pic2 from '../public/img/2.jpg'
+import pic3 from '../public/img/3.jpg'
+import pic4 from '../public/img/4.jpg'
+import pic5 from '../public/img/5.jpg'
+import pic6 from '../public/img/6.jpg'
 
 export default function Gallery() {
   return (
     <main>
       <div className="container">
         <div className="row">
-          <Card front={images[0]} back={images[3]} />
-          <Card front={images[1]} back={images[4]} />
-          <Card front={images[2]} back={images[5]} />
+          <Card front={pic1} back={pic4} />
+          <Card front={pic2} back={pic5} />
+          <Card front={pic3} back={pic6} />
         </div>
         <div className="row">
-          <Card front={images[3]} back={images[0]} />
-          <Card front={images[4]} back={images[1]} />
-          <Card front={images[5]} back={images[2]} />
+          <Card front={pic4} back={pic1} />
+          <Card front={pic5} back={pic2} />
+          <Card front={pic6} back={pic3} />
         </div>
       </div>
     </main>
