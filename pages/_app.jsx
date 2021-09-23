38ad578bce6d397c23656d50e7a9f4fdccaa94c2
Jan Delmar <jandelmardesign@gmail.com>
2021-09-23 21:47:55 +0200

    v5

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,9 +5,9 @@ import '../styles/global.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></Script>
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.7/js/tether.min.js"></Script>
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></Script>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" strategy="beforeInteractive" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.7/js/tether.min.js" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" />
       <Component {...pageProps} />
     </>
   )
